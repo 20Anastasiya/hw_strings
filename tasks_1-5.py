baf990d67c83d72dd85647718a37877817eeff4d
Anastasia Bereznevich <nastena.bereznevich@mail.ru>
2022-06-26 23:04:15 +0300

    strings

---
+++ tasks_1-5.py
@@ -0,0 +1,47 @@
+# letter = input('Введите строку:')
+# print('Введите строку:012345678')
+# print('третий символ этой строки:', letter[2])
+# print('предпоследний символ этой строки:', letter[-2])
+# print('первые пять символов этой строки:', letter[0:5])
+# print('вся строка, кроме последних двух символов:', letter[0:-2])
+# print('все символы с четными индексами:', letter[2::2])
+# print('все символы с нечетными индексами:', letter[1::2])
+# print('все символы в обратном порядке:', letter[::-1])
+# print('все символы строки через один в обратном порядке, начиная с последнего:', letter[::-2])
+# print('длина данной строки', len(letter))
+
+# print(' TASK 2:')     НЕ ВЕРНО!!!
+# first_letter = input('Введите 1-ую строку:')
+# second_letter = input('Введите 2-ую строку:')
+# if first_letter == second_letter[::-1]:
+#     print('две строки являются анаграммами')
+# else:
+#     print('две строки не являются анаграммами')
+
+# print(' TASK 3:')
+# string = input('Введите строку:')
+# symbol = input('Введите символ:')
+# print((string.lower()).count(symbol.lower()))
+
+# print(' TASK 4:')
+# string_4 = input('Введите строку:')
+# symbol_4 = input('Введите символ:')
+# print('индекс первого вхождения:', (string_4.lower()).find(symbol_4.lower()))
+# print('индекс второго вхождения:', (string_4.lower()).rfind(symbol_4.lower()))
+
+print(' TASK 5:')
+first_letter = input('Введите 1-ую строку:')
+second_letter = input('Введите 2-ую строку:')
+if first_letter.lower() == second_letter.lower()[::-1]:
+    print('две строки являются палиндромами')
+else:
+    print('две строки не являются палиндромами')
+
+
+
+
+
+
+
+
+
